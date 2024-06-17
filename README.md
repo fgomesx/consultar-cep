b5f44e73dbc53e194bc639400355cb0ce5463f07
Felipe-g22 <felipe.gomes_ti@outlook.com>
2024-06-16 21:37:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Consultar CEP
+Este projeto é uma aplicação web que permite buscar informações sobre um CEP (Código de Endereçamento Postal) no Brasil. A aplicação utiliza a API CEP V2 da [Brasil API](https://brasilapi.com.br/) para obter os dados e exibir as informações.
+
+## Tecnologias Utilizadas
+
+- JavaScript
+- HTML/CSS
+- React
+- Axios
+- API CEP V2 (Brasil API)
---
+++ src/App.js
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
+import { ReactTyped } from "react-typed";
+import './style.css';
+import api from "./services/api";
+
+// Componente para exibir as informações do cep com a animação
+const TypingText = ({ text }) => {
+  return (
+    <div>
+      <ReactTyped
+        style={{ whiteSpace: 'pre-line' }}
+        strings={[text]}
+        typeSpeed={5}
+        startDelay={0}
+        loop={false}
+        cursorChar={"_"}
+      />
+    </div>
+  );
+};
+
+function App() {
+
+  const [input, setInput] = useState(''); // Gerenciar o valor do input de CEP
+  const [cep, setCep] = useState({}); // Armazenar os dados do CEP retornados pela API
+  const [cepText, setCepText] = useState(''); // Armazenar o texto formatado com as informações do CEP
+
+  async function handleSearch() { // Buscar os dados do CEP na API
+    if (input === '') {
+      alert("Insira um CEP!")
+      return;
+    }
+
+    try { // Faz a requisição à API com o CEP fornecido
+      const response = await api.get(`${input}`);
+      setCep(response.data);
+
+      // As informações do CEP para exibição
+      const text = `
+        Local: ${response.data.city} - ${response.data.state} \n
+        Bairro: ${response.data.neighborhood} \n
+        Endereço: ${response.data.street} \n
+        Coordenadas: ${response.data.location.coordinates.latitude}, ${response.data.location.coordinates.longitude} \n
+        Fonte: ${response.data.service} 
+      `;
+
+      setCepText(text.trim());
+
+      setInput("");
+    } catch {
+      alert("CEP inválido!");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        <ReactTyped
+          strings={[`&lt;data.cep&gt;`, `Buscar CEP`]}
+          typeSpeed={30}
+          backSpeed={30}
+          backDelay={500}
+          loop={false}
+          cursorChar={"_"}
+        />
+      </h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite um CEP..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+
+        <main className="main">
+
+          <div className="window-header">
+            <div className="window-buttons">
+              <div className="window-button close"></div>
+              <div className="window-button minimize"></div>
+              <div className="window-button maximize"></div>
+
+            </div>
+            <p className="title-bar">CEP: {cep.cep}</p>
+            <div className="spacer"></div>
+          </div>
+
+          <div className="window-content">
+            <TypingText text={cepText} />
+          </div>
+
+        </main>
+      )}
+
+      <footer className="footer">
+        <p>Powered by FGOMESX |
+          <a href="https://github.com/fgomesx" target="_blank" rel="noopener noreferrer">
+            <FaGithub style={{ marginLeft: '5px' }} />
+          </a>
+        </p>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
